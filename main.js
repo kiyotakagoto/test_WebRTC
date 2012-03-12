477170746e09f8c6ed31ac1ab2b8ad01c73ec459
kiyotakagoto <spain.xavi.h@gmail.com>
2012-03-12 15:33:17 +0900

    capture bufferまでできた

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var CAPTURE = {};
+CAPTURE['interval'] = 200;
+CAPTURE['capture_buffer'] = []
+CAPTURE['capture_sequence'];
+
 var video = document.getElementById('live');
 
 navigator.webkitGetUserMedia(
@@ -6,6 +11,12 @@ navigator.webkitGetUserMedia(
     function ( stream ) {
         video.src = window.webkitURL.createObjectURL( stream );
         document.getElementById('msg').innerHTML = 'get video stream';
+        CAPTURE['capture_sequence'] = setInterval( function () {
+            if ( CAPTURE['capture_buffer'].length >= 5 ) {
+                CAPTURE['capture_buffer'].shift;
+            }
+            CAPTURE['capture_buffer'].push(video);
+        }, CAPTURE['interval']);
     },
     function ( error ) {
         console.log('Unable to get video stream');
@@ -13,6 +24,15 @@ navigator.webkitGetUserMedia(
 );
 
 document.getElementById('capture').onclick = function () {
-    var context = document.getElementById('canvas1').getContext('2d');
+    var context = document.getElementById('main').getContext('2d');
     context.drawImage(video, 0, 0);
+
+    for ( var i = 1; i <= CAPTURE['capture_buffer'].length; ++i ) {
+        var context = document.getElementById('sub' + i).getContext('2d');
+        context.drawImage( CAPTURE['capture_buffer'][i-1], 0, 0);
+        if ( i >= 5 ) {
+            break;
+        }
+    }
 };
+
